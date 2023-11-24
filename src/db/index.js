59de4eda851e682a401a45bcf93ce923aa18f4d5
animeshdesh <animeshdesh.aa@gmail.com>
2023-11-24 17:17:01 +0530

    Started Server

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 import { DB_NAME } from "../constant.js";
 
+// Always remember to run the DB connection function in async way because
+// as server is situated in different continent it make take some time to
+// connect to it and so it is also important to use try catch
+
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
@@ -10,7 +14,6 @@ const connectDB = async () => {
     console.log(
       `\n MongoDB Connected !! Host Name: ${connectionInstance.connection.host}`
     );
-    // console.log(connectionInstance);
   } catch (error) {
     console.log("MONGODB Connection FAILED", error);
     process.exit(1);
